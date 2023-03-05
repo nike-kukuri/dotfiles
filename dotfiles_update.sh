72c5e7a6f64e2229c32b189079e853f5dee870c0
nikekukuri <nikekukuri0327@gmail.com>
2023-03-05 13:56:39 +0900

    tmux.conf update

--- dotfiles_update.sh
+++ dotfiles_update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cp -f ~/.vimrc ~/dotfiles/vim
+cp -f ~/.vimrc ~/dotfiles/vim/vimrc
 cp -rf ~/.vim ~/dotfiles/vim
 cp -rf ~/.emacs.d ~/dotfiles/emacs
 cp -f ~/.zshrc ~/dotfiles/zsh/zshrc
 cp -f ~/.zprofile ~/dotfiles/zsh/zprofile
 cp -f ~/.bashrc ~/dotfiles/bash
-cp -f ~/.tmux.conf ~/dotfiles/tmux
-cp -f ~/.tmux.conf.mac ~/dotfiles/tmux
+cp -f ~/.tmux.conf ~/dotfiles/tmux.conf
+cp -f ~/.tmux.conf.mac ~/dotfiles/tmux.conf.mac
