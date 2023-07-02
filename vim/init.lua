73acde377bfd79f235f5747e3f883c8884b855aa
nikekukuri <nikekukuri0327@gmail.com>
2023-07-02 16:20:25 +0900

    vim: init.lua: add plugin 'dial.nvim'

--- vim/init.lua
+++ vim/init.lua
@@ -903,6 +903,20 @@ require("lazy").setup({
       }
   },
   {
+    'monaqa/dial.nvim',
+    config = function()
+      local dial = require("dial.map")
+      nmap("<C-a>", dial.inc_normal())
+      nmap("<C-x>", dial.dec_normal())
+      nmap("g<C-a>", dial.inc_normal())
+      nmap("g<C-a>", dial.dec_normal())
+      vmap("<C-a>", dial.inc_visual())
+      vmap("<C-x>", dial.dec_visual())
+      vmap("g<C-a>", dial.inc_gvisual())
+      vmap("g<C-x>", dial.dec_gvisual())
+    end
+  },
+  {
     'lambdalisue/gin.vim'
   },
   {
