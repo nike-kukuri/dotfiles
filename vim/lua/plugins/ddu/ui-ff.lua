a00c0dfd420f688c5d8a2e4203dde268a505f734
nikekukuri <nikekukuri0327@gmail.com>
2023-08-07 19:32:35 +0900

    vim: migrate: vim script to lua

--- vim/lua/plugins/ddu/ui-ff.lua
+++ vim/lua/plugins/ddu/ui-ff.lua
@@ -4,7 +4,7 @@ return {
     dependencies = "Shougo/ddu.vim",
     config = function()
       local vimx = require('artemis')
-      nmap('<Leader>zf', '<Cmd>call ddu#start({})<CR>')
+      vim.keymap.set('n', '<Leader>zf', '<Cmd>call ddu#start({})<CR>')
 
       vimx.fn.ddu.custom.patch_global({
         ui = 'ff',
@@ -24,46 +24,61 @@ return {
         },
       })
 
-vim.cmd([[
-call ddu#custom#patch_global({
-    \   'ui': 'ff',
-    \   'sources': [{'name': 'file_rec', 'params': {}}],
-    \   'sourceOptions': {
-    \     '_': {
-    \       'matchers': ['matcher_substring'],
-    \     },
-    \   },
-    \   'kindOptions': {
-    \     'file': {
-    \       'defaultAction': 'open',
-    \     },
-    \   }
-    \ })
+      vimx.fn.ddu.custom.patch_global({
+       uiParams = {
+         ff = {
+           startFilter = true,
+           split = 'floating',
+           prompt = "> ",
+           floatingBorder = "single",
+           filterFloatingPosition = "top",
+           autoAction = { name = "preview" },
+           startAutoAction = true,
+           previewFloating = true,
+           previewFloatingBorder = "rounded",
+           previewSplit = "vertical",
+           previewFloatingTitle = "Preview",
+           highlights = {
+             floating = "Normal",
+             floatingBorder = "Normal"
+           },
+         },
+       },
+      })
 
-call ddu#custom#patch_global({
-    \   'uiParams': {
-    \     'ff': {
-    \       'startFilter': v:true,
-    \       'split': 'floating',
-    \       'prompt': "> ",
-    \       'floatingBorder': "single",
-    \       'filterFloatingPosition': "top",
-    \       'autoAction': { 'name': "preview" },
-    \       'startAutoAction': v:true,
-    \       'previewFloating': v:true,
-    \       'previewFloatingBorder': "rounded",
-    \       'previewSplit': "vertical",
-    \       'previewFloatingTitle': "Preview",
-    \       'highlights': { 'floating': "Normal", 'floatingBorder': "Normal" },
-    \       'winCol': &columns / 9,
-    \       'winWidth': &columns / 10 * 3,
-    \       'winRow': &lines / 8,
-    \       'winHeight': &lines / 10 * 9,
-    \       'previewWidth': &columns / 10 * 5,
-    \       'previewHeight': &lines / 10 * 9,
-    \     },
-    \   },
-    \ })
+      local resize = function()
+        local width = math.floor(vim.o.columns * 0.8)
+        local previewWidth = math.floor(width * 0.5)
+        local height = math.floor(vim.o.lines * 0.8)
+        local previewHeight = height - 2
+        local row = math.floor((vim.o.lines - height) / 2)
+        local previewRow = row + 1
+        local col = math.floor(vim.o.columns * 0.1)
+        local halfWidth = math.floor(vim.o.columns * 0.5)
+        local previewCol = halfWidth - 2
+
+        vimx.fn.ddu.custom.patch_global({
+          uiParams = {
+            ff = {
+              winWidth = width,
+              winHeight = height,
+              winRow = row,
+              winCol = col,
+              previewWidth = previewWidth,
+              previewHeight = previewHeight,
+              previewRow = previewRow,
+              previewCol = previewCol,
+            },
+          },
+        })
+      end
+      vim.api.nvim_create_autocmd({ 'VimResized' }, {
+        pattern = { '*' },
+        callback = resize,
+      })
+      resize()
+
+vim.cmd([[
 
 function! s:ddu_my_settings() abort
   nnoremap <buffer><silent> <CR>
@@ -81,3 +96,9 @@ endfunction
   },
 }
 
+    --\       'winCol': &columns / 9,
+    --\       'winWidth': &columns / 10 * 3,
+    --\       'winRow': &lines / 8,
+    --\       'winHeight': &lines / 10 * 9,
+    --\       'previewWidth': &columns / 10 * 5,
+    --\       'previewHeight': &lines / 10 * 9,
