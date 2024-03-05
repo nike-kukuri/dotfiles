7b91bc4ea054ccd8d43ab6713dd75935c8872a6f
nikekukuri <nikekukuri0327@gmail.com>
2024-03-05 14:34:07 +0900

    Add(nvim): plugin 'vim-edgemotion'

--- vim/lua/plugins/util.lua
+++ vim/lua/plugins/util.lua
@@ -7,6 +7,15 @@ g['quickrun_config'] = {
 
 return {
   {
+    'haya14busa/vim-edgemotion',
+    config = function()
+      nmap('<M-j>', '<Plug>(edgemotion-j)')
+      nmap('<M-k>', '<Plug>(edgemotion-k)')
+      nmap('<M-j>', '<Plug>(edgemotion-j)')
+      nmap('<M-k>', '<Plug>(edgemotion-k)')
+    end
+  },
+  {
     'tani/vim-artemis'
   },
   {
