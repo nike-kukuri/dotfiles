dbbbb072a10bde953f2facce66e32464f42f221f
nikekukuri <nikekukuri0327@gmail.com>
2024-05-01 18:25:50 +0900

    Update(nvim): noice.nvim setup

--- vim/lua/plugins/ui.lua
+++ vim/lua/plugins/ui.lua
@@ -3,19 +3,57 @@ return {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
       messages = {
         view = "mini",
         view_error = "mini",
         view_warn = "mini",
       },
-      --popupmenu = {
-      --  backend = "cmp"
-      --},
+      popupmenu = {
+        enabled = true,
+				backend = "nui", -- "cmp" or "nui"
+      },
       notify = {
-        enabled = false,
-        view = "notify",
-      }
-      -- add any options here
+        enabled = true,
+        view = "mini",
+      },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = 5,
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = "auto",
+          },
+        },
+        popupmenu = {
+          relative = "editor",
+          position = {
+            row = 8,
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = 10,
+          },
+          border = {
+            style = "rounded",
+            padding = { 0, 1 },
+          },
+          win_options = {
+            winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+          },
+        },
+      },
     },
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
@@ -24,7 +62,14 @@ return {
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
       "rcarriga/nvim-notify",
-    }
+    },
+    enabled = function()
+      if vim.fn.has('mac') then
+        return false
+      else
+        return true
+      end
+    end
   },
 
   {
