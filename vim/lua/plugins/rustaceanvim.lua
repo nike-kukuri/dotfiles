5ef279361459a129fbef487187d054bd42591726
nikekukuri <nikekukuri0327@gmail.com>
2024-05-20 11:15:09 +0900

    Add(nvim): 'crates.nvim' plugin

--- vim/lua/plugins/rustaceanvim.lua
+++ vim/lua/plugins/rustaceanvim.lua
@@ -81,4 +81,11 @@ return {
     ft = { 'rust' },
     config = config,
   },
+  {
+    'saecki/crates.nvim',
+    tag = 'stable',
+    config = function()
+        require('crates').setup()
+    end,
+  }
 }
