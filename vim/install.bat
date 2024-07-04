238f708e95d8990b02918f0536bba7e87813798b
nikekukuri <nikekukuri0327@gmail.com>
2024-07-04 23:09:00 +0900

    Fix: skk link

--- vim/install.bat
+++ vim/install.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
-:: please, run by admin, and after install `uutils` ln -fs ~\dotfiles\vim\windows\init.lua ~\AppData\Local\nvim\init.lua
+:: please, run by admin, and after install `uutils` ln -fs ~\dotfiles\vim\init.lua ~\AppData\Local\nvim\init.lua
 
 goto :main
 
@@ -20,6 +20,7 @@ set DOTFILES=%USERPROFILE%\dotfiles
 
 call :link %USERPROFILE%\AppData\Local\nvim\init.lua %DOTFILES%\vim\init.lua
 call :dlink %USERPROFILE%\AppData\Local\nvim\lua %DOTFILES%\vim\lua
+call :dlink %USERPROFILE%\.config\skk %DOTFILES%\vim\skk
 
 endlocal
 pause
