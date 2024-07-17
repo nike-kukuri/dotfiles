8d84e6e49f7fddcdbe013c74b0f6e0f55b45063e
nikekukuri <nikekukuri0327@gmail.com>
2024-07-17 22:56:15 +0900

    Add: plugin markdown.nvim

--- vim/lua/plugins/util.lua
+++ vim/lua/plugins/util.lua
@@ -7,6 +7,19 @@ g['quickrun_config'] = {
 
 return {
   {
+    'MeanderingProgrammer/markdown.nvim',
+    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    config = function()
+        require('render-markdown').setup({
+          heading = { '#', '##', '###', '####', '#####', '######' },
+          checkbox = { '[ ]', '[x]' },
+        })
+    end,
+  },
+  {
     "akinsho/toggleterm.nvim",
 		event = "VimEnter",
     config = function()
