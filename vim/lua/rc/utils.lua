2fa7b8fd910423bb0dd4c5dd85cba7f8eb8102b9
nikekukuri <nikekukuri0327@gmail.com>
2024-08-05 23:42:03 +0900

    Add(nvim): util is_wsl function.

--- vim/lua/rc/utils.lua
+++ vim/lua/rc/utils.lua
@@ -1,10 +1,22 @@
 M = {}
 
 function M.create_cmd(name, impl, options)
-    if options == nil then
-        options = {}
+  if options == nil then
+    options = {}
+  end
+  vim.api.nvim_create_user_command(name, impl, options)
+end
+
+function M.is_wsl()
+  if vim.fn.has('wsl') then
+    return true
+  else
+    if vim.fn.has('win32') then
+      return true
+    else
+      return false
     end
-    vim.api.nvim_create_user_command(name, impl, options)
+  end
 end
 
 return M
