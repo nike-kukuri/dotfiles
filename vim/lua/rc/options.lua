fb36922a1178afbc9975882d16a0884e5ad0fda4
nikekukuri <nikekukuri0327@gmail.com>
2024-08-16 21:55:46 +0900

    Format

--- vim/lua/rc/options.lua
+++ vim/lua/rc/options.lua
@@ -1,34 +1,34 @@
-g.mapleader      = " "
-opt.breakindent  = true
-opt.number       = false
-opt.incsearch    = true
-opt.ignorecase   = true
-opt.smartcase    = true
-opt.hlsearch     = true
-opt.autoindent   = true
-opt.smartindent  = true
-opt.virtualedit  = "block"
-opt.showtabline  = 1
-opt.tabstop      = 2
-opt.shiftwidth   = 2
-opt.softtabstop  = 2
-opt.completeopt  = 'menu,menuone,noselect'
-opt.laststatus   = 3
-opt.scrolloff    = 10
-opt.cursorline   = true
-opt.helplang     = 'ja'
-opt.autowrite    = true
-opt.swapfile     = false
-opt.showtabline  = 1
+g.mapleader     = " "
+opt.breakindent = true
+opt.number      = false
+opt.incsearch   = true
+opt.ignorecase  = true
+opt.smartcase   = true
+opt.hlsearch    = true
+opt.autoindent  = true
+opt.smartindent = true
+opt.virtualedit = "block"
+opt.showtabline = 1
+opt.tabstop     = 2
+opt.shiftwidth  = 2
+opt.softtabstop = 2
+opt.completeopt = 'menu,menuone,noselect'
+opt.laststatus  = 3
+opt.scrolloff   = 10
+opt.cursorline  = true
+opt.helplang    = 'ja'
+opt.autowrite   = true
+opt.swapfile    = false
+opt.showtabline = 1
 -- opt.diffopt   = 'vertical,internal'
 -- opt.wildcharm = ('<Tab>'):byte()
-opt.tabstop      = 2
-opt.shiftwidth   = 2
-opt.softtabstop  = 2
-opt.fileformats  = 'unix,dos'
-opt.grepprg      = 'rg --vimgrep'
-opt.grepformat   = '%f:%l:%c:%m'
-opt.mouse        = 'a'
+opt.tabstop     = 2
+opt.shiftwidth  = 2
+opt.softtabstop = 2
+opt.fileformats = 'unix,dos'
+opt.grepprg     = 'rg --vimgrep'
+opt.grepformat  = '%f:%l:%c:%m'
+opt.mouse       = 'a'
 opt.clipboard:append({ fn.has('mac') == 1 and 'unnamed' or 'unnamedplus' })
 opt.termguicolors = true
 
@@ -45,7 +45,7 @@ vim.g.clipboard = {
   cache_enabled = 1,
 }
 
-cmd[[
+cmd [[
 if has('win32')
   if executable('pwsh')
     set shell=pwsh
@@ -57,7 +57,8 @@ endif
 -- for toggleterm.nvim
 if fn.has('win32') then
   if not fn.has('wsl') then
-    opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+    opt.shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
     opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
     opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
     opt.shellquote = ""
